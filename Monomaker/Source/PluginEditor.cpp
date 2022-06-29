24bfacdd91911f8207d51b06dcbe088256ff56be
LBiedermann <54768086+LBiedermann@users.noreply.github.com>
2022-06-29 11:34:12 +0200

    GraphicsTutorial

--- Monomaker/Source/PluginEditor.cpp
+++ Monomaker/Source/PluginEditor.cpp
@@ -15,7 +15,24 @@ MonomakerAudioProcessorEditor::MonomakerAudioProcessorEditor (MonomakerAudioProc
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (420, 420);
+    bgImage = ImageCache::getFromMemory(BinaryData::background_png, BinaryData::background_pngSize);
+
+
+    knob1.addListener(this);
+    knob1.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    knob1.setBounds(50, 50, 200, 200);
+    knob1.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+    knob1.setLookAndFeel(&largeKnob);
+    addAndMakeVisible(knob1);
+
+
+    knob2.addListener(this);
+    knob2.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    knob2.setBounds(85, 300, 100, 100);
+    knob2.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+    knob2.setLookAndFeel(&smallKnob);
+    addAndMakeVisible(knob2);
 }
 
 MonomakerAudioProcessorEditor::~MonomakerAudioProcessorEditor()
@@ -26,15 +43,15 @@ MonomakerAudioProcessorEditor::~MonomakerAudioProcessorEditor()
 void MonomakerAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawImageAt(bgImage, 0, 0);
 }
 
 void MonomakerAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+}
+
+void MonomakerAudioProcessorEditor::sliderValueChanged(Slider * slider) {
+
 }
--- Monomaker/Source/PluginEditor.h
+++ Monomaker/Source/PluginEditor.h
@@ -10,11 +10,13 @@
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include "Components/LargeKnob.h"
+#include "Components/SmallKnob.h"
 
 //==============================================================================
 /**
 */
-class MonomakerAudioProcessorEditor  : public juce::AudioProcessorEditor
+class MonomakerAudioProcessorEditor  : public juce::AudioProcessorEditor, Slider::Listener
 {
 public:
     MonomakerAudioProcessorEditor (MonomakerAudioProcessor&);
@@ -23,11 +25,21 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    void sliderValueChanged(Slider* slider) override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     MonomakerAudioProcessor& audioProcessor;
+
+    Image bgImage;
+
+    Slider knob1, knob2;
+    //Classes for LookAndFeel
+    LargeKnob largeKnob;
+    SmallKnob smallKnob;
+
+    LookAndFeel_V4 lookAndFeelV3;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MonomakerAudioProcessorEditor)
 };
